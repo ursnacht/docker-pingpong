430941d6360e50cab724d0b01f53428c57ccc1db
Urs Nacht <urs.nacht@glue.ch>
2019-02-01 09:00:53 +0100

    Added WebTarget

--- src/main/java/ch/glue/ping/boundary/ZnueniService.java
+++ src/main/java/ch/glue/ping/boundary/ZnueniService.java
@@ -3,6 +3,9 @@ package ch.glue.ping.boundary;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
@@ -14,26 +17,29 @@ import org.eclipse.microprofile.faulttolerance.Retry;
 @Stateless
 public class ZnueniService {
 
-    @Inject
-    @ConfigProperty(name = "message", defaultValue = "not configured")
-    String message;
+	@Inject
+	@ConfigProperty(name = "message", defaultValue = "not configured")
+	String message;
 
-    @Inject
-    WurstMetric metric;
+	@Inject
+	WurstMetric metric;
 
-    @Fallback(fallbackMethod = "veggy")
-    @Retry(maxRetries = 2)
-    public String message() {
-        System.out.println(".");
-        this.metric.nextWurst();
-        throw new NoWurstException("today is 21");
-        //return "42";
-    }
+	@Fallback(fallbackMethod = "veggy")
+	@Retry(maxRetries = 2)
+	public String message() {
+		System.out.println(".");
+		this.metric.nextWurst();
+		throw new NoWurstException("today is 21");
+		// return "42";
+	}
 
-    public String veggy() {
-        return "green stuff " + this.message;
-    }
+	public String veggy() {
+		return "green stuff " + this.message;
+	}
 
-
+	public String call(String host) {
+		WebTarget target = ClientBuilder.newClient().target(host);
+		return target.path("resource/ping/{call}").resolveTemplate("call", "meine url").request().get(String.class);
+	}
 
 }
