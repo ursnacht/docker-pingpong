d7fd74bcf04a653c266e36c9fa4b68c13d2fb75a
Urs Nacht <urs.nacht@glue.ch>
2019-02-28 15:03:57 +0100

    Removed unused network shell scripts

--- network.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-docker network create \
-	  --driver=bridge \
-	  --subnet=172.1.1.0/16 \
-	  pingpong-1
-
-#	  --ip-range=172.1.1.0/24 \
-#	  --gateway=172.1.1.254 \
-	  
--- second/network-2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-docker network create \
-	  --driver=bridge \
-	  --subnet=172.2.2.0/16 \
-	  pingpong-2
-	  
-#	  --ip-range=172.2.2.0/24 \
-#	  --gateway=172.2.2.254 \
-	  
